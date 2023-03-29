6789d35132937bb110c723a04a47ae7b8a37cdf4
Elias Sanchez Chara <elias.sanchez.chara@gmail.com>
2023-03-28 23:29:45 -0400

    Add changes

--- MisLugares/src/main/java/com/example/mislugares/GeoPunto.java
+++ MisLugares/src/main/java/com/example/mislugares/GeoPunto.java
@@ -1,2 +1,64 @@
-package PACKAGE_NAME;public class GeoPunto {
+package com.example.mislugares;
+
+import java.util.Objects;
+
+public class GeoPunto {
+    private double longitud, latitud;
+
+    static public GeoPunto SIN_POSICION = new GeoPunto(0.0, 0.0);
+
+    public GeoPunto(Double longitud, double latitud) {
+        this.longitud = longitud;
+        this.latitud = latitud;
+    }
+
+    @Override
+    public String toString() {
+        return new String("GeoPunto{" +
+                "longitud=" + longitud +
+                ", latitud=" + latitud +
+                '}');
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GeoPunto geoPunto = (GeoPunto) o;
+        return Double.compare(geoPunto.getLongitud(), getLongitud()) == 0 && Double.compare(geoPunto.getLatitud(), getLatitud()) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getLongitud(), getLatitud());
+    }
+
+    public double distancia(GeoPunto punto) {
+        final double RADIO_TIERRA = 6371000; // En metros
+        double dLat = Math.toRadians(latitud - punto.latitud);
+        double dLon = Math.toRadians(longitud - punto.longitud);
+        double lat1 = Math.toRadians(punto.latitud);
+        double lat2 = Math.toRadians(latitud);
+        double a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.sin(dLon/2) * Math.sin(dLon/2) *
+                    Math.cos(lat1) * Math.cos(lat2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+        return c * RADIO_TIERRA;
+    }
+
+    public double getLongitud() {
+        return longitud;
+    }
+
+    public void setLongitud(double longitud) {
+        this.longitud = longitud;
+    }
+
+    public double getLatitud() {
+        return latitud;
+    }
+
+    public void setLatitud(double latitud) {
+        this.latitud = latitud;
+    }
 }
